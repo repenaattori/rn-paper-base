64833f2926a00da8523ca450fab3b222dc0507ed
repsukka <81526112+repsukka@users.noreply.github.com>
2022-09-20 16:33:42 +0300

    init

---
+++ styles/Fonts.js
@@ -0,0 +1,60 @@
+import { configureFonts, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+
+const fontConfig = {
+    web: {
+      regular: {
+        fontFamily: 'sans-serif',
+        fontWeight: 'normal',
+      },
+      medium: {
+        fontFamily: 'sans-serif-medium',
+        fontWeight: 'normal',
+      },
+      light: {
+        fontFamily: 'sans-serif-light',
+        fontWeight: 'normal',
+      },
+      thin: {
+        fontFamily: 'sans-serif-thin',
+        fontWeight: 'normal',
+      },
+    },
+    ios: {
+      regular: {
+        fontFamily: 'sans-serif',
+        fontWeight: 'normal',
+      },
+      medium: {
+        fontFamily: 'sans-serif-medium',
+        fontWeight: 'normal',
+      },
+      light: {
+        fontFamily: 'sans-serif-light',
+        fontWeight: 'normal',
+      },
+      thin: {
+        fontFamily: 'sans-serif-thin',
+        fontWeight: 'normal',
+      },
+    },
+    android: {
+      regular: {
+        fontFamily: 'sans-serif',
+        fontWeight: 'normal',
+      },
+      medium: {
+        fontFamily: 'sans-serif-medium',
+        fontWeight: 'normal',
+      },
+      light: {
+        fontFamily: 'sans-serif-light',
+        fontWeight: 'normal',
+      },
+      thin: {
+        fontFamily: 'sans-serif-thin',
+        fontWeight: 'normal',
+      },
+    }
+  };
+
+  export default fontConfig;
---
+++ styles/Theme.js
@@ -0,0 +1,20 @@
+  
+  import { configureFonts, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+  import fontConfig from './Fonts';
+
+
+  const theme = {
+    ...DefaultTheme,
+    fonts: configureFonts(fontConfig),
+    roundness: 4,
+    colors:{
+        ...DefaultTheme.colors,
+        primary: "#6200EE",
+        accent: "#03DAC6",
+        primaryVariant: '#3700B3',
+        accentVariant: '#018766'
+    }
+  };
+
+  export default theme;
+  
